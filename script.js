2bcb81b0420081292b62849e0297b4afc27684dd
dunt21 <suadmacaulay50@gmail.com>
2025-03-06 17:46:25 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+"use strict";
+const mainCard = document.querySelector(".main-card");
+const thankUCard = document.querySelector(".popup-thankU");
+const btnSubmit = document.querySelector(".submitBtn");
+const btnOption = document.querySelectorAll(".btn-option");
+const optionSelected = document.querySelector(".optionSelected");
+const select = true;
+
+for (const btn of btnOption) {
+  btn.addEventListener("click", function () {
+    if (btn.classList.contains("selected")) {
+      btn.classList.remove("selected");
+    } else {
+      for (const unselect of btnOption) {
+        unselect.classList.remove("selected");
+      }
+      btn.classList.add("selected");
+    }
+  });
+}
+
+btnSubmit.addEventListener("click", function () {
+  const selectedBtn = document.querySelector(".selected");
+  if (selectedBtn) {
+    mainCard.style.display = "none";
+    thankUCard.style.display = "block";
+    optionSelected.textContent = optionSelected.textContent.replace(
+      "4",
+      selectedBtn.textContent
+    );
+    console.log(selectedBtn.textContent);
+  }
+});
